1efd2a5a906a0d5d8e99fc7d27f49f883cb6e9d1
MFuentes01 <megax.yt.uy@gmail.com>
2023-08-06 22:00:59 -0300

    Actividad terminada

---
+++ eventos.js
@@ -0,0 +1,6 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("contenedor").addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    })
+});
