65338e86a96e329daadfae14e597a4925f0cd80d
MFuentes01 <megax.yt.uy@gmail.com>
2023-08-08 20:19:49 -0300

    solucion-click-final

--- eventos.js
+++ eventos.js
@@ -1,6 +1,16 @@
 
 document.addEventListener("DOMContentLoaded", function() {
+    
+    
     document.getElementById("contenedor").addEventListener("click", function() {
         alert("Hola! Soy el div");
-    })
+    });
+
+    document.getElementById("btn").addEventListener("click", function(event) {
+        event.stopPropagation();
+    });
+    
+    
 });
+
+
